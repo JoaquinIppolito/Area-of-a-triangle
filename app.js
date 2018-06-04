31848a771b3782cb01dfe08f269e70ef161689c4
joaquin Ippolito <joaquin_nob_24@hotmail.com>
2018-06-04 12:48:21 -0300

    Fixing

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var TriangleArea = function()
 {
- var base = parsefloat(document.getElementById('base').value)
- var height = parsefloat(document.getElementById('height').value)
+ var base = parsefloat(document.getElementById('Base').value)
+ var height = parsefloat(document.getElementById('Height').value)
  var output = document.getElementById('output')
+ var TriangleArea = document.getElementById('Area')
  if(isNaN(base) || isNaN(height))
  {
   output.textContent = "No ingresaste 2 numeros."
  }
  else
  {
-  var Area = '0.5' * base * height;
-
-  output.textContent = "Area = " + TriangleArea ;
+  var Area = '0.5' * Base * Height
+  console.log(Area);
+  output.textContent = Area
  }
-
-
-};
+}
